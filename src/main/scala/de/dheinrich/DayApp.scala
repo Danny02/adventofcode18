516fbf2783e823a8fb7f57711a199f8c7e33786e
Daniel Heinrich <daniel.heinrich2@arbeitsagentur.de>
2018-12-12 17:51:48 +0100

    part of day 12 solution

--- src/main/scala/de/dheinrich/DayApp.scala
+++ src/main/scala/de/dheinrich/DayApp.scala
@@ -4,7 +4,6 @@ import fastparse.NoWhitespace._
 import fastparse._
 
 import scala.io.Source
-import fastparse._
 
 class DayApp(day: Int, val useExample: Boolean = false) extends App {
 
@@ -15,11 +14,16 @@ class DayApp(day: Int, val useExample: Boolean = false) extends App {
     .getLines()
     .toIndexedSeq
 
-  def parseInput[T](parser: P[_] => P[T]) = input.map(s => parse(s, parser).fold({
-    case (msg, index, extra) => throw new RuntimeException(extra.trace().longAggregateMsg)
-  }, {
-    case (t, i) => t
-  }))
+  def parseInput[T](parser: P[_] => P[T]) = input.map(parseLine(parser, _))
+
+  def parseLine[T](parser: P[_] => P[T], s: String) = {
+    parse(s, parser).fold({
+      case (msg, index, extra) =>
+        throw new RuntimeException(extra.trace().longAggregateMsg)
+    }, {
+      case (t, i) => t
+    })
+  }
 
   def number[_: P] = P(("-".? ~ CharIn("0-9").rep(1)).!.map(_.toInt))
 }
